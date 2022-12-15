9e70a6a5d7296856abae0333c757ac5ab1c67563
Miskiewicz <piotr.miskiewicz@sap.com>
2022-12-15 08:25:48 +0100

    wip

--- create_pr.sh
+++ create_pr.sh
@@ -5,5 +5,5 @@ git config --global user.email "ci@void"
 git checkout -b ${{BR_NAME}}
 git commit -a -m "Automatic PR"
 echo "Pushing"
-git push
+git push --set-upstream origin ${{BR_NAME}}
 gh pr create --title "The image bump" --body "Automatic docker image bump"
