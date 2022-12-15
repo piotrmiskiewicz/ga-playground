a73e52c38659838f40cf94247cdd374fea173c50
CI <ci@void>
2022-12-15 08:27:06 +0100

    Automatic PR

--- create_pr.sh
+++ create_pr.sh
@@ -1,9 +1,9 @@
 COMMIT_HASH=`git rev-parse HEAD`
-BR_NAME="autobump-${{COMMIT_HASH}}"
+BR_NAME="autobump-$COMMIT_HASH"
 git config --global user.name "CI"
 git config --global user.email "ci@void"
-git checkout -b ${{BR_NAME}}
+git checkout -b $BR_NAME
 git commit -a -m "Automatic PR"
 echo "Pushing"
-git push --set-upstream origin ${{BR_NAME}}
+git push --set-upstream origin $BR_NAME
 gh pr create --title "The image bump" --body "Automatic docker image bump"
