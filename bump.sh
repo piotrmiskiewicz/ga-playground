5f659f3de7bef1c4c4414a2dc8995af6f4f39eab
Piotr Miskiewicz <piotr.miskiewicz@sap.com>
2022-12-22 11:54:02 +0100

    wip

--- bump.sh
+++ bump.sh
@@ -1,3 +1,3 @@
-
-cat values.yaml | sed -e "s/tag.*$/tag: $1/" > v.yaml
-mv v.yaml values.yaml
+export file=chart/new-chart/values.yaml
+cat $file | sed -e "s/tag.*$/tag: $1/" > tmp.yaml
+mv tmp.yaml $file
